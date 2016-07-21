cb48c2a06a1e9b4f0769ea0c11be338b9f3dd406
finix <jinfei21@users.noreply.github.com>
2016-07-21 20:55:47 +0800

    Update HttpSimpleClient.java

--- src/main/java/com/yjfei/queue/util/HttpSimpleClient.java
+++ src/main/java/com/yjfei/queue/util/HttpSimpleClient.java
@@ -1,1 +1,157 @@
-package com.yjfei.queue.util;import java.io.IOException;import java.io.UnsupportedEncodingException;import java.net.HttpURLConnection;import java.net.URL;import java.net.URLEncoder;import java.util.Iterator;import java.util.List;public class HttpSimpleClient {    //    public static void main(String[] args) throws Exception {    //        HttpURLConnection conn = null;    //        try {    //            conn = (HttpURLConnection) new URL("http://jmenv.tbsite.net:8082/siteip").openConnection();    //            conn.setRequestMethod("GET");    //            conn.setConnectTimeout(3000);    //            conn.setReadTimeout(3000);    //                //            conn.connect();    //            int respCode = conn.getResponseCode(); // 锟斤拷锟斤拷锟节诧拷锟斤拷锟斤拷锟斤拷锟斤拷    //            String resp = null;    //    //            if (HttpURLConnection.HTTP_OK == respCode) {    //                resp = IOUtils.toString(conn.getInputStream());    //            } else {    //                resp = IOUtils.toString(conn.getErrorStream());    //            }    //            System.out.println(respCode);    //            System.out.println(resp);    //        } finally {    //            if (conn != null) {    //                conn.disconnect();    //            }    //        }    //    }    static public HttpResult httpGet(String url, List<String> headers, List<String> paramValues, String encoding,                                     long readTimeoutMs) throws IOException {        String encodedContent = encodingParams(paramValues, encoding);        url += (null == encodedContent) ? "" : ("?" + encodedContent);        HttpURLConnection conn = null;        try {            conn = (HttpURLConnection) new URL(url).openConnection();            conn.setRequestMethod("GET");            conn.setConnectTimeout(100);            conn.setReadTimeout((int) readTimeoutMs);            setHeaders(conn, headers, encoding);            conn.connect();            int respCode = conn.getResponseCode(); // 锟斤拷锟斤拷锟节诧拷锟斤拷锟斤拷锟斤拷锟斤拷            String resp = null;            if (HttpURLConnection.HTTP_OK == respCode) {                resp = IOUtils.toString(conn.getInputStream(), encoding);            } else {                resp = IOUtils.toString(conn.getErrorStream(), encoding);            }            return new HttpResult(respCode, resp);        } finally {            if (conn != null) {                conn.disconnect();            }        }    }    static public HttpResult httpPost(String url, List<String> headers, List<String> paramValues, String encoding,                                      long readTimeoutMs) throws IOException {        String encodedContent = encodingParams(paramValues, encoding);        HttpURLConnection conn = null;        try {            conn = (HttpURLConnection) new URL(url).openConnection();            conn.setRequestMethod("POST");            conn.setConnectTimeout(3000);            conn.setReadTimeout((int) readTimeoutMs);            conn.setDoOutput(true);            conn.setDoInput(true);            setHeaders(conn, headers, encoding);            conn.getOutputStream().write(encodedContent.getBytes());            int respCode = conn.getResponseCode(); // 锟斤拷锟斤拷锟节诧拷锟斤拷锟斤拷锟斤拷锟斤拷            String resp = null;            if (HttpURLConnection.HTTP_OK == respCode) {                resp = IOUtils.toString(conn.getInputStream(), encoding);            } else {                resp = IOUtils.toString(conn.getErrorStream(), encoding);            }            return new HttpResult(respCode, resp);        } finally {            if (null != conn) {                conn.disconnect();            }        }    }    static private void setHeaders(HttpURLConnection conn, List<String> headers, String encoding) {        if (null != headers) {            for (Iterator<String> iter = headers.iterator(); iter.hasNext();) {                conn.addRequestProperty(iter.next(), iter.next());            }        }        conn.addRequestProperty("Client-Version", "3.5.0"); // TODO        conn.addRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + encoding);    }    static private String encodingParams(List<String> paramValues, String encoding) throws UnsupportedEncodingException {        StringBuilder sb = new StringBuilder();        if (null == paramValues) {            return null;        }        for (Iterator<String> iter = paramValues.iterator(); iter.hasNext();) {            sb.append(iter.next()).append("=");            sb.append(URLEncoder.encode(iter.next(), encoding));            if (iter.hasNext()) {                sb.append("&");            }        }        return sb.toString();    }    static public class HttpResult {        final public int    code;        final public String content;        public HttpResult(int code, String content) {            this.code = code;            this.content = content;        }    }}
+public class HttpSimpleClient {
+	
+	
+	private static Proxy proxy;
+	
+	public static void initProxy(String host,int port){
+		 proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8080));
+	}
+
+	public static HttpResult httpGet(String url, List<String> headers,
+			List<String> paramValues, String encoding, long readTimeout)
+			throws IOException {
+
+		String encodedContent = encodingParams(paramValues, encoding);
+
+		url += (null == encodedContent) ? "" : ("?" + encodedContent);
+
+		HttpURLConnection conn = null;
+
+
+
+		try {
+			if(proxy == null){
+				conn = (HttpURLConnection) new URL(url).openConnection();
+			}else{
+				conn = (HttpURLConnection) new URL(url).openConnection(proxy);
+			}
+			conn.setRequestMethod("GET");
+			conn.setConnectTimeout(100);
+			conn.setReadTimeout((int) readTimeout);
+			setHeaders(conn, headers, encoding);
+
+			conn.connect();
+			int respCode = conn.getResponseCode();
+
+			String resp = null;
+
+			if (HttpURLConnection.HTTP_OK == respCode) {
+				resp = IOUtils.toString(conn.getInputStream(), encoding);
+			} else {
+				resp = IOUtils.toString(conn.getErrorStream(), encoding);
+			}
+
+			return new HttpResult(respCode, resp);
+
+		} finally {
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
+
+
+	}
+	
+	
+	public static HttpResult httpPost(String url ,List<String> headers,
+			List<String> paramValues, String encoding, long readTimeout)
+			throws IOException {
+		
+		String encodedContent = encodingParams(paramValues, encoding);
+
+		HttpURLConnection conn = null;
+
+		try {
+			conn = (HttpURLConnection) new URL(url).openConnection();
+			conn.setRequestMethod("GET");
+			conn.setConnectTimeout(100);
+			conn.setReadTimeout((int) readTimeout);
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			setHeaders(conn, headers, encoding);
+			
+			
+			conn.getOutputStream().write(encodedContent.getBytes());
+			
+			int respCode = conn.getResponseCode();
+			
+			String resp = null;
+
+			if (HttpURLConnection.HTTP_OK == respCode) {
+				resp = IOUtils.toString(conn.getInputStream(), encoding);
+			} else {
+				resp = IOUtils.toString(conn.getErrorStream(), encoding);
+			}
+
+			return new HttpResult(respCode, resp);
+			
+		}finally{
+			if (conn != null) {
+				conn.disconnect();
+			}
+		}
+		
+	}
+
+	private static void setHeaders(HttpURLConnection conn,
+			List<String> headers, String encoding) {
+		if (null != headers) {
+
+			for (Iterator<String> it = headers.iterator(); it.hasNext();) {
+				conn.addRequestProperty(it.next(), it.next());
+			}
+		}
+		conn.addRequestProperty("Content-Type",
+				"application/x-www-form-urlencode;charset=" + encoding);
+		conn.addRequestProperty("Client-Version", "3.5.0");
+
+	}
+
+	private static String encodingParams(List<String> paramValues,
+			String encoding) throws UnsupportedEncodingException {
+
+		if (null == paramValues) {
+			return null;
+		}
+		StringBuilder sb = new StringBuilder();
+		for (Iterator<String> it = paramValues.iterator(); it.hasNext();) {
+			sb.append(it.next()).append("=");
+			sb.append(URLEncoder.encode(it.next(), encoding));
+			if (it.hasNext()) {
+				sb.append("&");
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static class HttpResult {
+		public final int code;
+		public final String content;
+
+		public HttpResult(int code, String content) {
+			this.code = code;
+			this.content = content;
+		}
+	}
+	
+	
+	
+	public static void main(String args[]){
+		
+		String url = "http://api.wallstreetcn.com/v2/livenews";
+		
+		try {
+			
+			HttpSimpleClient.initProxy("10.59.103.238", 8080);
+			HttpResult result = HttpSimpleClient.httpGet(url, null, null, "utf-8", 50000L);
+			
+			
+			System.out.println(result.content);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
